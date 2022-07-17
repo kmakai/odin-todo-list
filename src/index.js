76c845f9e5bf9e8766852491ed99dc48fb1ca11c
kmakai <makaikwag@gmail.com>
2022-07-17 13:32:27 -0400

    4 files modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Project from './project.js';
 
-const t = new Project('tester');
+const t = new Project('Tester');
 t.addItem('test', 'test disc', 'test due', 'test notes');
 t.addItem('test2', 'test disc', 'test due', 'test notes');
 t.addItem('test3', 'test disc', 'test due', 'test notes');
@@ -13,9 +13,23 @@ console.log(projects);
 
 const pcont = document.querySelector('.projects-container');
 
-projects.forEach((p)=> {
-  const div = document.createElement("div");
-  div.className = "project"
-  div.innerHTML = `<h2>${p.title}</h2><span><i class="fa-regular fa-xmark"></i></span>`;
+projects.forEach((p) => {
+  const div = document.createElement('div');
+  div.className = 'project';
+  div.innerHTML = `<h2>${p.title}</h2><span>remove</span>`;
+
+  div.addEventListener('click', () => {
+    document.querySelector('.main').innerHTML = '';
+    p.items.forEach((item) => {
+      const divItem = document.createElement('div');
+      divItem.className = 'item';
+      divItem.innerHTML = `<h3 id="title>${item.title}</h3>
+  <p id="disc">${item.description}</p>
+  <p id="due-date">${item.due}</p>
+  <p id="notes">${item.notes}</p>`;
+      
+      document.querySelector('.main').append(divItem);
+    });
+  });
   pcont.append(div);
-})
+});
