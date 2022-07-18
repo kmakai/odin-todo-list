0572d6dda220c9e299faa987d76b068abafe3e4b
kmakai <makaikwag@gmail.com>
2022-07-17 21:10:06 -0400

    working on DOM

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
-import Project from './project.js';
+import Project from "./project.js";
 
-const t = new Project('Tester');
-t.addItem('test', 'test disc', 'test due', 'test notes');
-t.addItem('test2', 'test disc', 'test due', 'test notes');
-t.addItem('test3', 'test disc', 'test due', 'test notes');
-t.addItem('test4', 'test disc', 'test due', 'test notes');
-t.removeItem(1);
+const t = new Project("Tester");
+t.addItem("test", "test disc", "11/18/2022", "test notes");
+t.addItem("test2", "test disc", "11/18/2022", "test notes");
+t.addItem("test3", "test disc", "11/18/2022", "test notes");
+t.addItem("test4", "test disc", "11/18/2022", "test notes");
 console.log(t);
 
 const projects = [t, t, t, t];
 console.log(projects);
 
-const pcont = document.querySelector('.projects-container');
+const pcont = document.querySelector(".projects-container");
 
-projects.forEach((p) => {
-  const div = document.createElement('div');
-  div.className = 'project';
-  div.innerHTML = `<h2>${p.title}</h2><span>remove</span>`;
+const displayProjects = function () {
+  projects.forEach((p, i) => {
+    const div = document.createElement("div");
+    div.className = "project";
+    div.id = i;
+    div.innerHTML = `<h2>${p.title}</h2><span>remove</span>`;
 
-  div.addEventListener('click', () => {
-    document.querySelector('.main').innerHTML = '';
-    p.items.forEach((item) => {
-      const divItem = document.createElement('div');
-      divItem.className = 'item';
-      divItem.innerHTML = `<h3 id="title>${item.title}</h3>
-  <p id="disc">${item.description}</p>
-  <p id="due-date">${item.due}</p>
-  <p id="notes">${item.notes}</p>`;
-      
-      document.querySelector('.main').append(divItem);
+    div.addEventListener("click", () => {
+      document.querySelector(".main").innerHTML = "";
+
+      p.items.forEach((item) => {
+        const divItem = document.createElement("div");
+        divItem.className = "item";
+        divItem.innerHTML = `<input type="checkbox" id="mark"/><h3 id="title">${item.title}</h3>
+      <p id="disc">${item.discription}</p>
+      <p id="due-date">${item.due}</p>`;
+
+        document.querySelector(".main").append(divItem);
+      });
     });
+
+    pcont.append(div);
   });
-  pcont.append(div);
-});
+};
+
+displayProjects();
